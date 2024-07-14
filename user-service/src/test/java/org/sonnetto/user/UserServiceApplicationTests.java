a557a48d1d69c39635a6fc7e0f2273e724fb7a58
w4t3rcs <w4t3rofficial@gmail.com>
2024-07-14 23:41:41 +0200

    A little bit updated User Service

--- user-service/src/test/java/org/sonnetto/user/UserServiceApplicationTests.java
+++ user-service/src/test/java/org/sonnetto/user/UserServiceApplicationTests.java
@@ -1,13 +1,11 @@
-package com.sonnetto.user;
+package org.sonnetto.user;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UserServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
