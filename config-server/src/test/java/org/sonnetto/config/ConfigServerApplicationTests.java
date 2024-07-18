f67e1252d997ce1d303e60f36411ac06710afb46
w4t3rcs <w4t3rofficial@gmail.com>
2024-07-18 14:31:58 +0200

    Added Config Server

---
+++ config-server/src/test/java/org/sonnetto/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sonnetto.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
