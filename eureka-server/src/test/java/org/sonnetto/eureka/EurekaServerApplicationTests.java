f9891d0d50fc07a15e8d1582dcb3fc30a1c92e20
w4t3rcs <w4t3rofficial@gmail.com>
2024-07-18 14:31:29 +0200

    Added Eureka service registry as eureka-server module

---
+++ eureka-server/src/test/java/org/sonnetto/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sonnetto.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
