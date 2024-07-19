7f02ecab477ea807dc8245a3798cbe257d7a6744
w4t3rcs <w4t3rofficial@gmail.com>
2024-07-19 23:23:43 +0200

    Created api-gateway module

---
+++ api-gateway/src/test/java/org/sonnetto/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sonnetto.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
