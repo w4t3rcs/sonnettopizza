c4b581f7e7a2a9213504d82f1cf89a201c9edcfe
w4t3rcs <w4t3rofficial@gmail.com>
2024-07-21 22:41:38 +0200

    Finished k8s deployment including start.sh file for starting minikube

---
+++ start.sh
@@ -0,0 +1,9 @@
+minikube start
+minikube status
+
+cd ./k8s/ || exit
+kubectl apply -f ./mysql/
+kubectl apply -f ./eureka-server/
+kubectl apply -f ./config-server/
+kubectl apply -f ./user-service/
+kubectl apply -f ./api-gateway/
