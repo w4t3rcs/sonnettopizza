66c742a8a5c17e7e21981d462bbc55e633f1d40d
w4t3rcs <w4t3rofficial@gmail.com>
2024-07-22 09:40:13 +0200

    Returned docker-compose.yaml

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
-minikube start
 mvn clean compile jib:build
-cd ./k8s/ || exit
-kubectl apply -f ./mysql/
-kubectl apply -f ./eureka-server/
-kubectl apply -f ./config-server/
-kubectl apply -f ./user-service/
-kubectl apply -f ./api-gateway/
+#minikube start
+#cd ./k8s/ || exit
+#kubectl apply -f ./mysql/
+#kubectl apply -f ./eureka-server/
+#kubectl apply -f ./config-server/
+#kubectl apply -f ./user-service/
+#kubectl apply -f ./api-gateway/
+docker compose up --abort-on-container-exit
