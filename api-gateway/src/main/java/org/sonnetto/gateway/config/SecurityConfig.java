70efe357350af1e391816f1fb09c38683c2246ee
w4t3rcs <w4t3rofficial@gmail.com>
2024-07-22 23:03:43 +0200

    Started configuring security aspect of SonnettoPizza app in api-gateway module

---
+++ api-gateway/src/main/java/org/sonnetto/gateway/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package org.sonnetto.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+}
