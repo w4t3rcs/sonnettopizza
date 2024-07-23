9ecbe49433080ec6d05c846ef74770520b89e652
w4t3rcs <w4t3rofficial@gmail.com>
2024-07-23 21:28:36 +0200

    Configured SecurityWebFilterChain in SecurityConfig

--- api-gateway/src/main/java/org/sonnetto/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/org/sonnetto/gateway/config/SecurityConfig.java
@@ -1,9 +1,23 @@
 package org.sonnetto.gateway.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+        return httpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchangeSpec -> exchangeSpec.pathMatchers("/eureka/**")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated())
+                .oauth2ResourceServer(serverSpec -> serverSpec.jwt(Customizer.withDefaults()))
+                .build();
+    }
 }
