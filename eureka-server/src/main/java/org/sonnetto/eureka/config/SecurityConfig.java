81f918e9c627b9abffa14ed72d1942ef0204b988
w4t3rcs <w4t3rofficial@gmail.com>
2024-07-23 23:17:32 +0200

    Updated security for eureka-server module

---
+++ eureka-server/src/main/java/org/sonnetto/eureka/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package org.sonnetto.eureka.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager authenticationManager(@Value("eureka.security.user") String user,
+                                                            @Value("eureka.security.password") String password) throws Exception {
+        return new InMemoryUserDetailsManager(User.withUsername(user)
+                .password(password)
+                .roles("USER")
+                .build());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(registry -> registry.anyRequest().authenticated())
+                .httpBasic(configurer -> configurer.init(httpSecurity))
+                .build();
+    }
+}
