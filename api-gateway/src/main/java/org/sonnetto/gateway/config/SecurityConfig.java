e277a7a4f30db24e633db0f9b719c053a5a4c438
w4t3rcs <w4t3rofficial@gmail.com>
2024-07-24 22:40:46 +0200

    Returned not working security for api-gateway (will be fixed soon)

--- api-gateway/src/main/java/org/sonnetto/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/org/sonnetto/gateway/config/SecurityConfig.java
@@ -1,29 +1,24 @@
 package org.sonnetto.gateway.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
-import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity, ReactiveJwtDecoder jwtDecoder) {
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
         return httpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchangeSpec ->
-//                        exchangeSpec.pathMatchers("/eureka/**")
-//                                .permitAll()
-//                                .anyExchange()
-//                                .authenticated())
-                        exchangeSpec.anyExchange()
-                                .permitAll())
-//                .oauth2ResourceServer(serverSpec -> serverSpec.jwt(Customizer.withDefaults()))
+                        exchangeSpec.pathMatchers("/eureka/**")
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated())
+                .oauth2ResourceServer(serverSpec -> serverSpec.jwt(Customizer.withDefaults()))
                 .build();
     }
 }
