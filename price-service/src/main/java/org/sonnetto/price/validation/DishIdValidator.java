192e11f775e67b204d4e40202b558a55d793036d
w4t3rcs <w4t3rofficial@gmail.com>
2024-07-27 02:01:47 +0200

    Fixed DishIdValidator

--- price-service/src/main/java/org/sonnetto/price/validation/DishIdValidator.java
+++ price-service/src/main/java/org/sonnetto/price/validation/DishIdValidator.java
@@ -4,24 +4,21 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import java.util.List;
 
-public class DishIdValidator implements ConstraintValidator<DishId, List<Long>> {
+public class DishIdValidator implements ConstraintValidator<DishId, Long> {
     @Override
-    public boolean isValid(List<Long> ids, ConstraintValidatorContext constraintValidatorContext) {
-        if (ids == null || ids.isEmpty()) return true;
-        return Boolean.TRUE.equals(Flux.fromIterable(ids)
-                .publishOn(Schedulers.boundedElastic())
-                .map(id -> WebClient.create("http://localhost:8083/api/v1.0/dishes/" + id)
-                        .get()
-                        .retrieve()
-                        .toBodilessEntity()
-                        .block()
-                        .getStatusCode()
-                        .is2xxSuccessful())
-                .all(aBoolean -> aBoolean)
-                .block());
+    public boolean isValid(Long id, ConstraintValidatorContext constraintValidatorContext) {
+        if (id == null) return false;
+        return Boolean.TRUE.equals(WebClient.create("http://localhost:8083/api/v1.0/dishes/" + id)
+                .get()
+                .retrieve()
+                .toBodilessEntity()
+                .block()
+                .getStatusCode()
+                .is2xxSuccessful());
     }
 }
