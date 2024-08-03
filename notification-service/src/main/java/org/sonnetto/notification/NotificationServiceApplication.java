749dc7e12efbaf94edd0e27b7ef62931fde61d74
w4t3rcs <w4t3rofficial@gmail.com>
2024-08-03 09:44:22 +0200

    Configuring kafka

--- notification-service/src/main/java/org/sonnetto/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/org/sonnetto/notification/NotificationServiceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NotificationServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
     }
-
 }
