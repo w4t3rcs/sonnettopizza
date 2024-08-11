56f87f510eed81f874a432a79379159983c02c99
w4t3rcs <w4t3rofficial@gmail.com>
2024-08-11 23:38:31 +0200

    Returning Security for api-gateway

--- api-gateway/src/main/java/org/sonnetto/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/org/sonnetto/gateway/config/SecurityConfig.java
@@ -1,24 +1,29 @@
 package org.sonnetto.gateway.config;
 
 //import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-//import org.springframework.security.config.web.server.ServerHttpSecurity;
-//import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-//@EnableWebFluxSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
-//    @Bean
-//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
-//        return httpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
-//                .authorizeExchange(exchangeSpec ->
-//                        exchangeSpec.pathMatchers("/eureka/**")
-//                                .permitAll()
-//                                .anyExchange()
-//                                .authenticated())
-//                .oauth2ResourceServer(serverSpec -> serverSpec.jwt(Customizer.withDefaults()))
-//                .build();
-//    }
+    @Value("${spring.security.unsecured-paths}")
+    private String[] unsecuredPaths;
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+        return httpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchangeSpec ->
+                        exchangeSpec.pathMatchers(unsecuredPaths)
+                                .permitAll()
+                                .anyExchange()
+                                .authenticated())
+                .oauth2ResourceServer(serverSpec -> serverSpec.jwt(Customizer.withDefaults()))
+                .build();
+    }
 }
