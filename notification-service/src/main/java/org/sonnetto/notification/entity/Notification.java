6a794de846ec1463c43eb865bf553a7578361e85
w4t3rcs <w4t3rofficial@gmail.com>
2024-08-12 09:39:09 +0200

    Replaced MySQL with Elasticsearch

--- notification-service/src/main/java/org/sonnetto/notification/entity/Notification.java
+++ notification-service/src/main/java/org/sonnetto/notification/entity/Notification.java
@@ -1,42 +1,18 @@
 package org.sonnetto.notification.entity;
 
-import jakarta.persistence.*;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.PastOrPresent;
 import lombok.*;
-import org.hibernate.proxy.HibernateProxy;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
-@Getter @Setter
+@Data
 @AllArgsConstructor @NoArgsConstructor
-@Entity
-@Table(name = "notifications")
+@Document(indexName = "notification")
 public class Notification implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
-    @Valid
-    @Embedded
     private Message message;
-    @PastOrPresent
     private LocalDateTime sentDate;
-
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
-        Notification that = (Notification) o;
-        return getId() != null && Objects.equals(getId(), that.getId());
-    }
-
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
-    }
 }
