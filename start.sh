1efa718249cdffa14fb6af73970a3e89b077d052
w4t3rcs <w4t3rofficial@gmail.com>
2024-08-18 23:50:10 +0200

    Deploying to k8s

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
-mvn clean compile jib:build
-#minikube start
-#cd ./k8s/ || exit
-#kubectl apply -f ./mysql/
-#kubectl apply -f ./eureka-server/
-#kubectl apply -f ./config-server/
-#kubectl apply -f ./user-service/
-#kubectl apply -f ./api-gateway/
-docker compose up --abort-on-container-exit
+# Pushing microservices onto docker registry hub
+mvn clean compile test jib:build
+# Creating k8s cluster using Kind (https://kind.sigs.k8s.io/)
+cd ./k8s/kind || exit
+kind create cluster --name=sonnetto-microservices --config kind-config.yaml
+# Creating needed infrastructure for microservices (for example, kafka, redis, mysql, etc)
+cd ../manifests/ || exit
+kubectl apply -f ./infrastructure/
+# Creating microservices
+cd ./application/ || exit
+kubectl apply -f ./eureka-server/
+kubectl apply -f ./config-server/
