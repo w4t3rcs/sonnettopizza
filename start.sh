8fd944630ca64644796b2f5ea89df481ae61ec11
w4t3rcs <w4t3rofficial@gmail.com>
2024-08-20 12:49:42 +0200

    K8s deploying

--- start.sh
+++ start.sh
@@ -3,12 +3,9 @@ mvn clean compile test jib:build
 # Creating k8s cluster using Kind (https://kind.sigs.k8s.io/)
 cd ./k8s/kind || exit
 kind create cluster --name=sonnetto-microservices --config kind-config.yaml
-# Creating needed infrastructure for microservices (for example, kafka, redis, mysql, etc)
-cd ../manifests/infrastructure/ || exit
-kubectl --context docker-desktop --namespace=default apply -f ./broker/
-kubectl --context docker-desktop --namespace=default apply -f ./mysql/
-kubectl --context docker-desktop --namespace=default apply -f ./redis/
-kubectl --context docker-desktop --namespace=default apply -f ./zipkin/
+# Creating needed infrastructure for microservices (kafka, redis, mysql, etc)
+cd ../manifests/ || exit
+kubectl --context docker-desktop --namespace=default apply -f ./infrastructure/
 # Creating microservices
 cd ../application/ || exit
 kubectl --context docker-desktop --namespace=default apply -f ./eureka-server/
