1f0fc2c8bcfb76247cb40fc192e597575f587e9b
w4t3rcs <w4t3rofficial@gmail.com>
2024-09-09 10:39:22 +0200

    Moved to /k8s/local directory

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 # Pushing microservices onto docker registry hub
 mvn clean compile test jib:build
 # Creating k8s cluster using Kind (https://kind.sigs.k8s.io/)
-cd ./k8s/kind || exit
+cd ./k8s/local/kind || exit
 kind create cluster --name=sonnetto-microservices --config kind-config.yaml
 # Deploying ECK
 kubectl create -f https://download.elastic.co/downloads/eck/2.14.0/crds.yaml
